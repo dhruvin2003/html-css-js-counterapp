70f7a2358cc60bf042aacb76d39b7b2ade6c3b04
DHRUV AHUJA <123013669+dhruvin2003@users.noreply.github.com>
2024-01-28 10:12:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-const Decrementbutton = document.getElementById("Decrementbutton");
-const Incrementbutton = document.getElementById("Incrementbutton");
-const Resetbutton = document.getElementById("Resetbutton");
-const Displayvalue = document.getElementById("Displayvalue");
-
-Decrementbutton.addEventListener("click", () => {
-    const value = Number(Displayvalue.innerText);
-    if (value > 0) {
-        Displayvalue.innerText = value - 1;
-
-    } else {
-        alert("Negative Value NOT Allowed")
-    }
-});
-
-Incrementbutton.addEventListener("click", () => {
-    const value = Number(Displayvalue.innerText);
-    if (value >= 10) {
-        alert("10+ Values Are NOT Allowed");
-
-
-
-    } else {
-        Displayvalue.innerText = value + 1;
-    }
-});
-
-Resetbutton.addEventListener("click", () => {
-    Displayvalue.innerText = 0;
+const boxelement = document.getElementById("box");
+// click event listner 
+boxelement.addEventListener("click", function() {
+    alert("areyy bhyii click kiya re");
 });
