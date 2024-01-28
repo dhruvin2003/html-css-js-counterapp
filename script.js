1c6bddb1c651ba4d0f86a90e569094e339874a4a
DHRUV AHUJA <123013669+dhruvin2003@users.noreply.github.com>
2024-01-28 12:15:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const boxelement = document.getElementById("box");
-// click event listner 
-boxelement.addEventListener("click", function() {
-    alert("areyy bhyii click kiya re");
+let container = document.getElementById("container");
+let display = document.getElementById("display");
+// adding keydown event listner
+document.addEventListener("keydown", function(e) {
+    display.style.color = "red";
+    display.innerText = e.key + "keydown";
+});
+// adding keyup event listner
+document.addEventListener("keyup", function(e) {
+    display.style.color = "green";
+    display.innerText = e.key + "keyup";
 });
