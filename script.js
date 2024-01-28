e539d3986907a406df685cde5558033868006443
DHRUV AHUJA <123013669+dhruvin2003@users.noreply.github.com>
2024-01-28 10:30:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const boxelement = document.getElementById("box");
-// click event listner 
-boxelement.addEventListener("click", function() {
-    alert("areyy bhyii click kiya re");
+// double click event listner
+boxelement.addEventListener("dblclick", function() {
+    alert("areyy ab double click kiya re");
 });
